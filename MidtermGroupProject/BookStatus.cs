d247c90acb76648f9fd155dc19517c2860abf7f8
Andrew <andrewpullen1@gmail.com>
2023-03-02 20:58:38 -0500

    added BookStatus enum

---
+++ MidtermGroupProject/BookStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidtermGroupProject
+{
+    enum BookStatus
+    {
+        OnShelf,
+        CheckedOut,
+        Late,
+        Reserved
+    }
+}
