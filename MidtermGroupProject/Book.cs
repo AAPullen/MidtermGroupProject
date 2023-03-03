7ee3c5d6186c02616c3c8b2792317e0aea8072d4
Andrew <andrewpullen1@gmail.com>
2023-03-02 21:17:30 -0500

    first commit

---
+++ MidtermGroupProject/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidtermGroupProject
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public BookStatus Status { get; set; }
+
+    }
+}
--- MidtermGroupProject/CheckoutReturn.cs
+++ MidtermGroupProject/CheckoutReturn.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace MidtermGroupProject
 {
-    public enum BookStatus
+    public class CheckoutReturn
     {
-        OnShelf,
-        CheckedOut,
-        Late,
-        Reserved
+        public void CheckOut(Book title)
+        {
+            
+        }
     }
 }
