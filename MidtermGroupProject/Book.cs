d4ef9e90957c842750b2460ddfd8e7645473cbd6
Lisa Van Huijkelom <lisa.vanhuijkelom@infinicept.com>
2023-03-02 19:56:30 -0700

    Work in progress....

---
+++ MidtermGroupProject/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidtermGroupProject
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public DateTime DueDate { get; set; }
+        //public enum Status { get; set; }  --> This is giving me an error for some reason and I need to try to figure out why
+     
+        
+        public Book(string _Title, string _Author, DateTime _DueDate)
+        {
+            Title = _Title;
+            Author = _Author;
+            DueDate = _DueDate;
+        }
+
+
+
+    }
+}
--- MidtermGroupProject/Program.cs
+++ MidtermGroupProject/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MidtermGroupProject;
+
+// book list class so I can create a list of the 12 books  --> Extra credit will go to storing it eventually in a text file and reading it from there // writing to it?
+
+List<Book> BookList = new List<Book>();
+
+BookList.Add(new Book("IT STARTS WITH US", "Colleen Hoover", DateTime.Now));
+BookList.Add(new Book("LESSONS IN CHEMISTRY", "Bonnie Garmus", DateTime.Now));
+BookList.Add(new Book("BURNER", "Mark Greaney", DateTime.Now));
+BookList.Add(new Book("SPARE", "Prince Harry", DateTime.Now));
+BookList.Add(new Book("THE LIGHT WE CARRY", "Michelle Obama", DateTime.Now));
+BookList.Add(new Book("TOMORROW, AND TOMORROW, AND TOMORROW", "Gabrielle Zevin", DateTime.Now));
+BookList.Add(new Book("I HAVE SOME QUESTIONS FOR YOU", "Rebecca Makkai", DateTime.Now));
+BookList.Add(new Book("SOMEONE ELSE'S SHOES", "Jojo Moyes", DateTime.Now));
+BookList.Add(new Book("IT ENDS WITH US", "Colleen Hoover", DateTime.Now));
+BookList.Add(new Book("HEART BONES", "Colleen Hoover", DateTime.Now));
+BookList.Add(new Book("ALL ABOUT LOVE", "Bell Hooks", DateTime.Now));
+BookList.Add(new Book("BRAIDING SWEETGRASS", "Robin Wall Kimmerer", DateTime.Now));
+BookList.Add(new Book("CASTE", "Isabel Wilkerson", DateTime.Now));
