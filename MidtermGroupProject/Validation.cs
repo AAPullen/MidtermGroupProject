7af9e1db5e6d92f5c0e4d6412e7f7f3b760915fe
Andrew <andrewpullen1@gmail.com>
2023-03-07 17:19:25 -0500

    added availibility check and user intent check

--- MidtermGroupProject/Validation.cs
+++ MidtermGroupProject/Validation.cs
@@ -9,10 +9,20 @@ namespace MidtermGroupProject
     internal class Validation
     {
         // Create a method to handle yes/no answers from the user
-        public bool IsValidAnswer(string answer)
+        public bool CheckAnswer(string answer)
         {
             answer = answer.ToLower();
             if (answer == "y" || answer == "n")
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool CheckIntent(string intent)
+        {
+            intent = intent.ToLower();
+            if (intent == "search" || intent == "return")
             {
                 return true;
             }
