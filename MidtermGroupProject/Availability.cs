47c2de67d0abc43929ab59d0947b5a30e8bfccef
Andrew <andrewpullen1@gmail.com>
2023-03-08 18:43:07 -0500

    added availability class

---
+++ MidtermGroupProject/Availability.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidtermGroupProject
+{
+    public class Availability
+    {
+        public bool CheckAvailability(Book book)
+        {
+            if (book.Status == BookStatus.BookStatuses.OnShelf)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
