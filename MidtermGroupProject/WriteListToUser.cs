09e5cbab6a6dc37e3e8a6d0bb83861f14b9482a3
Billy-McCleese <billymccleese@gmail.com>
2023-03-09 21:31:18 -0500

    WriteListToUser

---
+++ MidtermGroupProject/WriteListToUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidtermGroupProject
+{
+    public class WriteListToUser
+    {
+       public void WriteListToConsole(List<Book> Book) 
+        {
+            foreach (var book in Book)
+            {
+              Console.WriteLine($"{book.Id}:  {book.Title} by {book.Author}\n");
+
+            }
+        }
+    }
+}
